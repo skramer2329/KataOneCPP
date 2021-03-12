3518b34653370200357c3dab678505ab54b444cf
skramer2329 <skramer2329@gmail.com>
2021-03-12 17:09:55 -0600

    Add project files.

---
+++ KataOneCPP/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+	//prints hello world
+	std::cout << "Hello, world!" << std::endl;
+	
+	//This will wait for user to type a character like enter
+	std::cin.get(); 
+}
